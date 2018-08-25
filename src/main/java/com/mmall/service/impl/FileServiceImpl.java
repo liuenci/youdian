3cceca4372c14c95fea3fc9764aec14825020102
liuenci <154910381@qq.com>
2018-08-25 21:55:13 +0800

    封装了 jedis 工具类

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -13,22 +13,22 @@ import java.util.UUID;
 
 @Service("iFileService")
 @Slf4j
-public class FileServiceImpl implements IFileService{
+public class FileServiceImpl implements IFileService {
 
-    public String upload(MultipartFile file,String path){
+    public String upload(MultipartFile file, String path) {
         String fileName = file.getOriginalFilename();
         // 扩展名
         // abc.jpg
-        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".")+1);
-        String uploadFileName = UUID.randomUUID().toString()+"."+fileExtensionName;
-        log.info("开始上传文件，上传文件的文件名:{},上传的路径:{},新文件名:{}",fileName, path,uploadFileName);
+        String fileExtensionName = fileName.substring(fileName.lastIndexOf(".") + 1);
+        String uploadFileName = UUID.randomUUID().toString() + "." + fileExtensionName;
+        log.info("开始上传文件，上传文件的文件名:{},上传的路径:{},新文件名:{}", fileName, path, uploadFileName);
 
         File fileDir = new File(path);
-        if (!fileDir.exists()){
+        if (!fileDir.exists()) {
             fileDir.setWritable(true);
             fileDir.mkdirs();
         }
-        File targetFile = new File(path,uploadFileName);
+        File targetFile = new File(path, uploadFileName);
         try {
             file.transferTo(targetFile);
             //文件已经上传成功
@@ -36,7 +36,7 @@ public class FileServiceImpl implements IFileService{
             // 已经上传到ftp服务器上
             targetFile.delete();
         } catch (IOException e) {
-            log.error("上传文件异常",e);
+            log.error("上传文件异常", e);
             return null;
         }
         return targetFile.getName();
