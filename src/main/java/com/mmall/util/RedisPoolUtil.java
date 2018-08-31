dcc533f398228a96a3531575e1e30b73e28e138e
liuenci <154910381@qq.com>
2018-08-31 09:33:55 +0800

    新增 Redis Pool 工具类

---
+++ src/main/java/com/mmall/util/RedisPoolUtil.java
@@ -0,0 +1,132 @@
+package com.mmall.util;
+
+import com.mmall.common.RedisPool;
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+
+@Slf4j
+public class RedisPoolUtil {
+    /**
+     * 设置 key 的有效期，单位是秒
+     *
+     * @param key
+     * @param exTime
+     * @return
+     */
+    public static Long expire(String key, int exTime) {
+        Jedis jedis = null;
+        Long result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.expire(key, exTime);
+        } catch (Exception e) {
+            log.error("expire key:{} error", key, e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    /**
+     * 设置 key value 以及过期时间
+     * @param key
+     * @param value
+     * @param exTime
+     * @return
+     */
+    public static String setEx(String key, String value, int exTime) {
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.setex(key, exTime, value);
+        } catch (Exception e) {
+            log.error("setex key:{} value:{} error", key,value, e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    /**
+     * 设置 key value
+     * @param key
+     * @param value
+     * @return
+     */
+    public static String set(String key, String value) {
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.set(key, value);
+        } catch (Exception e) {
+            log.error("set key:{} value:{} error", key,value, e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        return result;
+    }
+
+    /**
+     * 得到 key 的 value
+     * @param key
+     * @return
+     */
+    public static String get(String key) {
+        Jedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.get(key);
+        } catch (Exception e) {
+            log.error("get key:{} error", key, e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        return result;
+    }
+
+    /**
+     * 删除 key
+     * @param key
+     * @return
+     */
+    public static Long del(String key) {
+        Jedis jedis = null;
+        Long result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.del(key);
+        } catch (Exception e) {
+            log.error("del key:{} error", key, e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Jedis jedis = RedisPool.getJedis();
+
+        RedisPoolUtil.set("keyTest","Value");
+
+        String value = RedisPoolUtil.get("keyTest");
+
+        RedisPoolUtil.setEx("keyex","valueex",60 * 10);
+
+        RedisPoolUtil.expire("keyTest",60 * 20);
+
+        RedisPoolUtil.del("keyTest");
+
+        System.out.println("end");
+    }
+
+}
