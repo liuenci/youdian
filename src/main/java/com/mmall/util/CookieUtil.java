50cbfb4cd9075bc7a4ab0ec4ac1598249c4cad8e
liuenci <154910381@qq.com>
2018-09-06 10:57:55 +0800

    cookie 工具类开发

---
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -0,0 +1,57 @@
+package com.mmall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CookieUtil {
+
+    private final static String COOKIE_DOMAIN = ".youdian.com";
+    private final static String COOKIE_NAME = "youdian.LOGIN_TOKEN";
+
+    public static String readLoginToken(HttpServletRequest request){
+        Cookie[] cks = request.getCookies();
+        if (cks != null){
+            for (Cookie ck : cks){
+                log.info("read cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
+                if (StringUtils.equals(ck.getName(),COOKIE_NAME)){
+                    log.info("return cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
+                    return ck.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void writeLoginToken(HttpServletResponse response,String token){
+        Cookie ck = new Cookie(COOKIE_NAME,token);
+        ck.setDomain(COOKIE_DOMAIN);
+        ck.setPath("/"); // 代表设置在根目录
+        ck.setHttpOnly(true);
+        // 单位是秒
+        // 如果这个 maxage 不设置的话，cookie 就不会写入硬盘，而是写在内存。只在当前页面有效
+        ck.setMaxAge(60 * 60 * 24 * 365);
+        log.info("write cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
+        response.addCookie(ck);
+    }
+
+    public static void delLoginToken(HttpServletRequest request,HttpServletResponse response){
+        Cookie[] cks = request.getCookies();
+        if (cks != null){
+            for (Cookie ck : cks){
+                if (StringUtils.equals(ck.getName(),COOKIE_NAME)){
+                    ck.setDomain(COOKIE_DOMAIN);
+                    ck.setPath("/");
+                    ck.setMaxAge(0); // 设置成 0,代表删除此 cookie
+                    log.info("del cookieName:{},cookieValue:{}",ck.getName(),ck.getValue());
+                    response.addCookie(ck);
+                    return;
+                }
+            }
+        }
+    }
+}
